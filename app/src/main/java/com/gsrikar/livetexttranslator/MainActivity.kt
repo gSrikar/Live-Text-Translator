84e1904cd188eff771639980c26f9bc2e8bda7dd
Srikar Reddy <gsrikar2010@gmail.com>
2019-05-17 00:29:41 +0530

    Set action, status bar, navigation to translucent and hide the toolbar text

--- app/src/main/java/com/gsrikar/livetexttranslator/MainActivity.kt
+++ app/src/main/java/com/gsrikar/livetexttranslator/MainActivity.kt
@@ -9,6 +9,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
+        // Hide the toolbar text
+        supportActionBar?.setDisplayShowTitleEnabled(false)
     }
 
 }
